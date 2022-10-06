68f5fe3d3529d2304d63cde0a895bebcde800a26
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2022-10-06 10:16:18 -0600

    total price command added

--- repl-cart.js
+++ repl-cart.js
@@ -1,61 +1,90 @@
-import rl from 'readline-sync';
+import rl from "readline-sync";
 
-let commands = ['list products', 'show product', 'add to cart'];
+let commands = [
+  "list products",
+  "show product",
+  "add to cart",
+  "show total price",
+];
 let products = [
   {
-    name: 'hair spray',
-    price: '$5',
-    description: 'keep your hair straight & tidy',
-    upcCode: '123xyz567',
+    name: "hair spray",
+    price: "$5",
+    description: "keep your hair straight & tidy",
+    upcCode: "123xyz567",
   },
   {
-    name: 'fruit loops',
-    price: '$3',
-    description: 'best cereal ever',
-    upcCode: '111yyy000',
+    name: "fruit loops",
+    price: "$3",
+    description: "best cereal ever",
+    upcCode: "111yyy000",
   },
   {
-    name: 'tweezers',
-    price: '$1',
-    description: 'Tweek, OW!',
-    upcCode: '333398988',
+    name: "tweezers",
+    price: "$1",
+    description: "Tweek, OW!",
+    upcCode: "333398988",
   },
   {
-    name: 'bananananananas',
-    price: '$29',
-    description: 'Yellow and Yummy',
-    upcCode: '4011',
+    name: "bananananananas",
+    price: "$29",
+    description: "Yellow and Yummy",
+    upcCode: "4011",
   },
   {
-    name: 'baseball cards',
-    price: '$0.10',
+    name: "baseball cards",
+    price: "$0.10",
     description: "Let's Go Blue Jays",
     upcCode: null,
   },
 ];
 
 let cart = [];
+let totalPrice = 0;
 
 while (true) {
-  console.log('\n----------------\nThe commands are:', commands);
-  const theCommand = rl.question('What is your command? ');
+  console.log("\n----------------\nThe commands are:", commands);
 
-  if (theCommand === 'list products') {
-    console.log('Here is the list of products:');
-    products.forEach((p) => {console.log('  - ', p.name)})
-  } else if (theCommand === 'show product') {
-    const theName = rl.question('Which product do you want to see? ');
+  const theCommand = rl.question("What is your command? ");
+
+  if (theCommand === "list products") {
+    console.log("Here is the list of products:");
+    products.forEach((p) => {
+      console.log("  - ", p.name);
+    });
+  } else if (theCommand === "show product") {
+    const theName = rl.question("Which product do you want to see?");
     const theProduct = products.find((p) => p.name === theName);
-    console.log('The product details are:\n', theProduct);
-  } else if (theCommand === 'add to cart') {
-    const theName = rl.question('Which product do you want to add to cart? ');
+    console.log("The product details are:\n", theProduct);
+  } else if (theCommand === "add to cart") {
+    const theName = rl.question("Which product do you want to add to cart? ");
     const theProduct = products.find((p) => p.name === theName);
     cart.push(theProduct);
-    console.log('Added! Number of items in your cart: ', cart.length);
+    console.log("Added! Number of items in your cart: ", cart.length);
+  }
+  // Show total price
+  else if (theCommand === "show total price") {
+    totalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+      totalPrice = totalPrice + parseInt(cart[i].price.replace("$", ""));
+    }
+
+    console.log("Total Price: ", totalPrice);
+    const confirmation = rl.question("are you sure to purchase?");
+
+    if (confirmation === "yes") {
+      console.log("Enjoy your purchases!");
+      cart = [];
+    } else {
+      console.log("Go to your cart to purchase whenever you ready!");
+    }
   } else {
     console.log(`Invalid command: ${theCommand}`);
   }
 }
 
-
-
+/*TODO: To be added later
+ const result = rl.question("Do you want to continue?");
+   if (result === "No" || result === "no") {
+     break;
+     */
