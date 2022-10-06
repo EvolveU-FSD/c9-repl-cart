1bf494018571aeaf164c9cbe332cf1d656a401f9
Ashish Verma <113393491+the3rdeyecan@users.noreply.github.com>
2022-10-06 12:12:31 -0600

    Revert "added cart total functionality"
    This reverts commit 8aa6f305e865ecae2a7931a61ce803feb42a6ca0.

--- repl-cart.js
+++ repl-cart.js
@@ -6,36 +6,35 @@ let commands = [
   'add to cart',
   'show cart',
   'remove from cart',
-  'checkout',
 ];
 let products = [
   {
     name: 'hair spray',
-    price: 5,
+    price: '$5',
     description: 'keep your hair straight & tidy',
     upcCode: '123xyz567',
   },
   {
     name: 'fruit loops',
-    price: 2,
+    price: '$2',
     description: 'best cereal ever',
     upcCode: '111yyy000',
   },
   {
     name: 'tweezers',
-    price: 1,
+    price: '$1',
     description: 'Tweek, OW!',
     upcCode: '333398988',
   },
   {
     name: 'bananas',
-    price: 29,
+    price: '$29',
     description: 'Yellow and Yummy',
     upcCode: '4011',
   },
   {
     name: 'baseball cards',
-    price: 0.1,
+    price: '$0.10',
     description: "Let's Go Blue Jays",
     upcCode: null,
   },
@@ -60,7 +59,10 @@ while (true) {
     const theName = rl.question('\nWhich product do you want to add to cart? ');
     const theProduct = products.find((p) => p.name === theName);
     cart.push(theProduct);
-    console.log(`\n${theProduct.name} Added!` + '\nNumber of items in your cart: ', cart.length);
+    console.log(
+      `\n${theProduct.name} Added!` + '\nNumber of items in your cart: ',
+      cart.length
+    );
   } else if (theCommand === 'show cart') {
     if (cart.length === 0) {
       console.log('\nYour cart is empty!');
@@ -80,14 +82,6 @@ while (true) {
     const trashCart = cart.find((p) => p.name === itemToRemove);
     cart.shift(trashCart);
     console.log(`\n${trashCart.name} removed! `);
-  } else if (theCommand === 'checkout') {
-    console.log('\nAre you ready to checkout?');
-    console.log('\nYour cart total is: ');
-    let cartTotal = 0;
-    for (let index = 0; index < cart.length; index++) {
-      cartTotal += cart[index].price;
-    }
-    console.log(cartTotal);
   } else {
     console.log(`Invalid command: ${commands}`);
   }
